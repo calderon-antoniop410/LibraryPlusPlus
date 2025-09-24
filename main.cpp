6dea6362bd4bf38c356f721d239af5a41187c81a
Adrian Hernandez <Esnecpe356@gmail.com>
2025-09-24 13:30:50 -0700

    Fourth commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "bookSearch.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QListWidget>
+#include <QListWidgetItem>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    QListWidget listWidget;
+    w.resize(900, 600);
+    w.show();
+    return a.exec();
+}
